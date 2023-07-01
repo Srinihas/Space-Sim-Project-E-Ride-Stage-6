d676de07cec9b7a0028951cb6937b473fd7651f9
Srinihas <115796290+Srinihas@users.noreply.github.com>
2023-07-01 15:49:19 +0400

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,12 @@ import firebase from "firebase";
 require("@firebase/firestore");
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCZrG0BF6LrnRpWZjsPQxNGyMCLByo8HrM",
-  authDomain: "e-ride-549e4.firebaseapp.com",
-  projectId: "e-ride-549e4",
-  storageBucket: "e-ride-549e4.appspot.com",
-  messagingSenderId: "588298110700",
-  appId: "1:588298110700:web:34e3df331c6b1fc95047c9",
-  measurementId: "G-G3K7BWL5LH"
+  apiKey: "AIzaSyA36wH9WCnbv0tiBfXcx6_c4ewJ703f42o",
+  authDomain: "e-ride-cb9db.firebaseapp.com",
+  projectId: "e-ride-cb9db",
+  storageBucket: "e-ride-cb9db.appspot.com",
+  messagingSenderId: "890192014751",
+  appId: "1:890192014751:web:a367429556d30a47aae1aa"
 };
 
 firebase.initializeApp(firebaseConfig);
